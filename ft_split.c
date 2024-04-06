26560400b77ad257d6a4e7e0f3a8311d945d58b2
zekmaro <84675870+zekmaro@users.noreply.github.com>
2024-04-06 21:35:30 +0000

    added second block

--- ft_split.c
+++ ft_split.c
@@ -6,19 +6,18 @@
 /*   By: anarama <anarama@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 16:07:22 by anarama           #+#    #+#             */
-/*   Updated: 2024/04/06 21:20:49 by anarama          ###   ########.fr       */
+/*   Updated: 2024/04/06 23:05:34 by anarama          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 static int  helper_count_words(char const *s, char c)
 {
     int  count_words;
 
     count_words = 0;
-    if (*s != c)
+    if (*s != c && *s != '\0')
         count_words++;
     while (*s)
     {
@@ -29,58 +28,44 @@ static int  helper_count_words(char const *s, char c)
     return (count_words);
 }
 
-static void helper_terminate_strings(char *str, char c)
+static int  helper_len_substr(const char *s, char c)
 {
-    while(*str)
+    int i;
+    
+    i = 0;
+    while (*s != c)
     {
-        if (*str == c)
-            *str = '\0';
-        str++;
+        i++;
+        s++;
     }
+    return (i);
 }
 
 char    **ft_split(char const *s, char c)
 {
-    int  count_words;
-    char **arr;
-    char **temp_arr;
-
+    char    **arr;
+    int     count_words;
+    char    **temp_arr;
+    
     if (s == NULL)
         return (NULL);
     count_words = helper_count_words(s, c);
-    printf("%d", count_words);
-    arr = (char **)malloc(sizeof(char *) * count_words + 1);
+    arr = (char **)malloc(sizeof(char *) * (count_words + 1));
     if (!arr)
         return (NULL);
     temp_arr = arr;
-    if (*s != c)
-    {
-        *arr = ft_strdup(s);    
-        helper_terminate_strings(*arr, c);
-        arr++;
-    }
     while (*s)
     {
-        if (*s == c && *(s + 1) != c && *(s + 1) != '\0')
+        while (*s == c && *s)
+            s++;
+        if (*s != '\0')
         {
-            *arr = ft_strdup((s + 1));
-            helper_terminate_strings(*arr, c);
+            *arr = ft_substr(s, 0, helper_len_substr(s, c));
             arr++;
         }
-        s++;
+        while (*s != c && *s)
+            s++;
     }
-    arr = NULL;
+    *arr = NULL;
     return (temp_arr);
 }
-
-// int main(void)
-// {
-//     char *s = "\0aa\0bbb";
-//     char **result = ft_split(s, '\0');
-
-//     while (*result)
-//     {
-//         printf("%s\n", *result);
-//         result++;
-//     }
-// }
